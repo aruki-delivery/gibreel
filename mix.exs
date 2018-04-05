2ac616fbe9dd75a66d81b531362544406704da2d
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 18:51:59 +0100

    woot

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule GibReel.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :gibreel,
+      version: "0.1.0",
+      language: :erlang,
+      deps: deps(Mix.env()),
+      description: "Distributed cache implemented in Erlang",
+      package: package(),
+      source_url: "https://github.com/aruki-delivery/gibreel",
+      homepage_url: "https://hex.pm/packages/gibreel"]
+  end
+
+  defp deps(_) do
+    [{:columbo, "~> 0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  def package do
+    [ maintainers: ["cblage"],
+      licenses: ["Apache License 2.0"],
+      links: %{"GitHub" => "https://github.com/aruki-delivery/gibreel" } ]
+  end
+end
