38fd5b466d68decd06e3ecefd6061ae5394b3959
Carlos Brito Lage <carlos@carloslage.net>
2018-04-08 08:47:45 +0100

    bump

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "gibreel",
   description: "Distributed cache implemented in Elixir / Erlang",
-  version: "2.0.1",
+  version: "2.0.2",
   keywords: ["elixir", "erlang", "distributed", "cache"],
   licenses: [[name: "Apache License 2.0"]],
   maintainers: [
