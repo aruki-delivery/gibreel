48232b0db3fdf0085ad2ea95e79285a6a1809017
Carlos Brito Lage <carlos@carloslage.net>
2018-04-09 11:47:48 +0100

    erlang only

--- lib/gibreel/records/cache_record.ex
+++
@@ -1,6 +0,0 @@
-defmodule Gibreel.CacheRecord do
-  @moduledoc false
-  @fields [name: "none", config: %Gibreel.CacheConfig{}]
-  defstruct @fields
-  def fields, do: @fields
-end
--- mix.exs
+++ mix.exs
@@ -3,28 +3,27 @@ defmodule Gibreel.Mixfile do
 
   def project do
     [app: :gibreel,
-      version: "3.0.5",
+      version: "4.0.0",
+      language: :erlang,
       deps: deps(Mix.env()),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test],
-      description: "Distributed cache implemented in Elixir / Erlang",
+      description: "Distributed cache implemented in Erlang",
       package: package(),
       source_url: "https://github.com/aruki-delivery/gibreel",
       homepage_url: "https://hex.pm/packages/gibreel"]
   end
 
   defp deps(_) do
-    [{:swarm, "~> 3.0"},
+  [
      {:columbo, "~> 0.1.0"},
      {:cclock, "~> 0.1.0"},
      {:async, "~> 0.1.0"},
-     {:excoveralls, "~> 0.8", only: :test},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
-  def application do
-    [mod: {Gibreel.Application, []},
-      extra_applications: [:logger, :columbo],]
-  end
+
+  #def application do
+  #  [mod: {Gibreel.Application, []},
+  #    extra_applications: [:logger, :columbo],]
+  #end
 
   def package do
     [ maintainers: ["cblage"],
