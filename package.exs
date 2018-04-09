881ae78c721a27fa7dd109f08ff97022d9128982
Carlos Brito Lage <carlos@carloslage.net>
2018-04-09 11:48:32 +0100

    woot

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(
   name: "gibreel",
-  description: "Distributed cache implemented in Elixir / Erlang",
+  description: "Distributed cache implemented in Erlang",
   version: "4.0.0",
   keywords: ["elixir", "erlang", "distributed", "cache"],
   licenses: [[name: "Apache License 2.0"]],
