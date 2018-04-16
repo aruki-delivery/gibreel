964b3384a458d9b51cec6fa09698708a35a1c3b1
Carlos Brito Lage <carlos@carloslage.net>
2018-04-16 12:52:32 +0100

    fix compat

--- mix.exs
+++ mix.exs
@@ -3,11 +3,9 @@ defmodule Gibreel.Mixfile do
 
   def project do
     [app: :gibreel,
-      version: "6.0.0",
+      version: "6.0.1",
       language: :erlang,
       deps: deps(Mix.env()),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test],
       description: "Distributed cache implemented in Elixir / Erlang",
       package: package(),
       source_url: "https://github.com/aruki-delivery/gibreel",
@@ -20,10 +18,6 @@ defmodule Gibreel.Mixfile do
      {:cclock, "~> 2.0.0"},
      {:async, "~> 2.0.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
-  end
-  def application do
-    [mod: {Gibreel.Application, []},
-      extra_applications: [:logger, :columbo],]
   end
 
   def package do
